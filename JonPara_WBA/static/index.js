4cbc5f2a5106171262edac9af2915210f6d8f6b9
Jonathan Paradiso <33237727+JonPara@users.noreply.github.com>
2018-06-15 08:40:44 -0400

    Added map to home page. Finishing JS file map and tile markers

--- JonPara_WBA/static/index.js
+++ JonPara_WBA/static/index.js
@@ -1,17 +1,42 @@
-function createMap() {
+var capitalUrl = "http://techslides.com/demos/country-capitals.json"
 
-    // Define a satellite layer
-    var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" + 
-    "access_token=pk.eyJ1Ijoiam9ucGFyYSIsImEiOiJjamh4aXplemMwY2FiM3dscmgyYWx2cnA2In0.EkbMLM9pydlSlE8pBTjxLA")
+var map = L.map( 'map', {
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2
+});
 
-    // Define a dark map layer
-    var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1IjoidGJlcnRvbiIsImEiOiJjamRoanlkZXIwenp6MnFuOWVsbGo2cWhtIn0.zX40X0x50dpaN96rKQKarw." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoiam9ucGFyYSIsImEiOiJjamh4aXplemMwY2FiM3dscmgyYWx2cnA2In0.EkbMLM9pydlSlE8pBTjxLA", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ["a","b","c"]
+    }).addTo(map);
 
-    // Define a baseMaps objects to hold the base layer
-    var baseMaps = {
-        "Satellite Map": satelliteMap,
-        "Dark Map": darkMap
-    };
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+ 
+var myIcon = L.icon({
+iconUrl: myURL + 'images/pin24.png',
+iconRetinaUrl: myURL + 'images/pin48.png',
+iconSize: [29, 24],
+iconAnchor: [9, 21],
+popupAnchor: [0, -14]
+});
 
+var markerClusters = L.markerClusterGroup();
+ 
+for ( var i = 0; i < markers.length; ++i )
+{
+  var popup = markers[i].name +
+              '<br/>' + markers[i].city +
+              '<br/><b>IATA/FAA:</b> ' + markers[i].iata_faa +
+              '<br/><b>ICAO:</b> ' + markers[i].icao +
+              '<br/><b>Altitude:</b> ' + Math.round( markers[i].alt * 0.3048 ) + ' m' +
+              '<br/><b>Timezone:</b> ' + markers[i].tz;
+ 
+  var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+                  .bindPopup( popup );
+ 
+  markerClusters.addLayer( m );
+}
+ 
+map.addLayer( markerClusters );
